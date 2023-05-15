9b4d21b724b06610c2fe06cdfbe50d871125fa84
qzhang16 <qzhang16@ra.rockwell.com>
2023-05-15 13:51:12 +0800

    demo MapStruct. Lombok can't run with MapStruct correctly, so have to remove LomBok settings.

--- src/main/java/asg/dev/UserCommand.java
+++ src/main/java/asg/dev/UserCommand.java
@@ -1,11 +1,11 @@
 package asg.dev;
 
-public class User {
+public class UserCommand {
     private String firstName;
     private String lastName;
     private String email;
 
-    public User(String firstName, String lastName, String email) {
+    public UserCommand(String firstName, String lastName, String email) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
---
+++ src/main/java/asg/dev/UserMapper.java
@@ -0,0 +1,11 @@
+package asg.dev;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface UserMapper {
+    UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
+    UserCommand userToUserCommand(User user);
+    User userCommandToUser(UserCommand userCommand);
+}
